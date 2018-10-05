f36acc25ea70c5e6a982cb3c5a98661fe7381457
rudzikdawid <rudzikdawid@gmail.com>
2018-10-05 14:04:41 +0200

    Refactor demo, upgrade to polymer 3 code style based on class

--- demo/x-local-translate.js
+++ demo/x-local-translate.js
@@ -8,63 +8,65 @@ found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
 part of the polymer project is also subject to an additional IP rights grant
 found at http://polymer.github.io/PATENTS.txt
 */
-import '@polymer/polymer/polymer-legacy.js';
 import '@polymer/paper-toggle-button/paper-toggle-button.js';
 import './common-styles.js';
 
-import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
-import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js'
 
 import {AppLocalizeBehavior} from '../app-localize-behavior.js';
 
-Polymer({
-  _template: html`
-    <style include="common-styles"></style>
+class xLocalTranslate extends mixinBehaviors
+(AppLocalizeBehavior, PolymerElement) {
+  static get template() {
+    return html`
+      <style include="common-styles"></style>
 
-    <div class="lang">
-      <span title="english">🇬🇧 EN</span>
-      <paper-toggle-button on-change="_toggle" id="switch"></paper-toggle-button>
-      <span title="french">FR 🇫🇷</span>
-    </div>
-
-    <h4>This demo's resources are loaded statically, not from an external file.</h4>
-
-    <div class="snippet">
-      <div class="demo">
-        <div>{{localize('greeting')}}</div>
+      <div class="lang">
+        <span title="english">🇬🇧 EN</span>
+        <paper-toggle-button on-change="_toggle" id="switch"></paper-toggle-button>
+        <span title="french">FR 🇫🇷</span>
       </div>
-      <div class="code-container">
-        <code>localize('greeting')</code>
+  
+      <h4>This demo's resources are loaded statically, not from an external file.</h4>
+  
+      <div class="snippet">
+        <div class="demo">
+          <div>{{localize('greeting')}}</div>
+        </div>
+        <div class="code-container">
+          <code>localize('greeting')</code>
+        </div>
       </div>
-    </div>
-`,
+    `;
+  }
 
-  is: 'x-local-translate',
+  static get properties() {
+    return {
+      /* Overriden from AppLocalizeBehavior */
+      language: {value: 'en', type: String},
 
-  behaviors: [AppLocalizeBehavior],
+      /* Overriden from AppLocalizeBehavior */
+      formats: {
+        type: Object,
+        value: function() {
+          return {number: {USD: {style: 'currency', currency: 'USD'}}};
+        }
+      },
 
-  properties: {
-    /* Overriden from AppLocalizeBehavior */
-    language: {value: 'en', type: String},
-
-    /* Overriden from AppLocalizeBehavior */
-    formats: {
-      type: Object,
-      value: function() {
-        return {number: {USD: {style: 'currency', currency: 'USD'}}};
+      /* Overriden from AppLocalizeBehavior */
+      resources: {
+        type: Object,
+        value: function() {
+          return {'en': {'greeting': 'Hello!'}, 'fr': {'greeting': 'Bonjour!'}};
+        }
       }
-    },
+    };
+  }
 
-    /* Overriden from AppLocalizeBehavior */
-    resources: {
-      type: Object,
-      value: function() {
-        return {'en': {'greeting': 'Hello!'}, 'fr': {'greeting': 'Bonjour!'}};
-      }
-    }
-  },
-
-  _toggle: function() {
+  _toggle() {
     this.language = this.$.switch.checked ? 'fr' : 'en';
   }
-});
+}
+
+window.customElements.define('x-local-translate', xLocalTranslate);
--- demo/x-translate.js
+++ demo/x-translate.js
@@ -8,83 +8,90 @@ found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
 part of the polymer project is also subject to an additional IP rights grant
 found at http://polymer.github.io/PATENTS.txt
 */
-import '@polymer/polymer/polymer-legacy.js';
 import '@polymer/paper-toggle-button/paper-toggle-button.js';
 import './common-styles.js';
 
-import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
-import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js'
 
 import {AppLocalizeBehavior} from '../app-localize-behavior.js';
 
-Polymer({
-  _template: html`
-    <style include="common-styles"></style>
+class xTranslate extends mixinBehaviors
+(AppLocalizeBehavior, PolymerElement) {
+  constructor() {
+    super();
+  }
 
-    <div class="lang">
-      <span title="english">🇬🇧 EN</span>
-      <paper-toggle-button on-change="_toggle" id="switch"></paper-toggle-button>
-      <span title="french">FR 🇫🇷</span>
-    </div>
-
-    <h4>{{localize('header_1')}}</h4>
-    <div class="snippet">
-      <div class="demo">
-        <div>{{localize('greeting')}}</div>
-        <div>{{localize('missing')}}</div>
-        <div>{{localize('intro', 'name', 'Batman')}}</div>
-        <div>{{localize('cats', 'numCats', 10000, 'pctBlack', 0.42)}}</div>
-        <div>{{localize('sale', 'start', 150, 'time', 15, 'price', 10)}}</div>
+  static get template() {
+    return html`
+      <style include="common-styles"></style>
+      
+      <div class="lang">
+        <span title="english">🇬🇧 EN</span>
+        <paper-toggle-button on-change="_toggle" id="switch"></paper-toggle-button>
+        <span title="french">FR 🇫🇷</span>
       </div>
-      <div class="code-container">
-        <code>localize('greeting')</code>
-        <code>localize('intro', 'name', 'Batman')</code>
-        <code>localize('cats', 'numCats', 10000, 'pctBlack', 0.42)</code>
-        <!-- Dates need to be a valid JavaScript object. For an example, we
-          are passing a number of milliseconds from the epoch -->
-        <code>localize('sale', 'start', 150, 'time', 15, 'price', 10)</code>
+  
+      <h4>{{localize('header_1')}}</h4>
+      <div class="snippet">
+        <div class="demo">
+          <div>{{localize('greeting')}}</div>
+          <div>{{localize('missing')}}</div>
+          <div>{{localize('intro', 'name', 'Batman')}}</div>
+          <div>{{localize('cats', 'numCats', 10000, 'pctBlack', 0.42)}}</div>
+          <div>{{localize('sale', 'start', 150, 'time', 15, 'price', 10)}}</div>
+        </div>
+        <div class="code-container">
+          <code>localize('greeting')</code>
+          <code>localize('intro', 'name', 'Batman')</code>
+          <code>localize('cats', 'numCats', 10000, 'pctBlack', 0.42)</code>
+          <!-- Dates need to be a valid JavaScript object. For an example, we
+            are passing a number of milliseconds from the epoch -->
+          <code>localize('sale', 'start', 150, 'time', 15, 'price', 10)</code>
+        </div>
       </div>
-    </div>
-
-    <h4>{{localize('header_2')}}</h4>
-    <div class="snippet">
-      <div class="demo">
-        <div>{{localize('fruit', 'num', 0)}} / {{localize('fruit', 'num', 1)}}/ {{localize('fruit', 'num', 3)}}</div>
-        <div>{{localize('bananas', 'name', 'Robin', 'gender', 'male', 'num', 1)}}</div>
-        <div>{{localize('bananas', 'name', 'Robin', 'gender', 'female', 'num', 0)}}</div>
-        <div>{{localize('bananas', 'name', 'Robin', 'gender', 'other', 'num', 4)}}</div>
+  
+      <h4>{{localize('header_2')}}</h4>
+      <div class="snippet">
+        <div class="demo">
+          <div>{{localize('fruit', 'num', 0)}} / {{localize('fruit', 'num', 1)}}/ {{localize('fruit', 'num', 3)}}</div>
+          <div>{{localize('bananas', 'name', 'Robin', 'gender', 'male', 'num', 1)}}</div>
+          <div>{{localize('bananas', 'name', 'Robin', 'gender', 'female', 'num', 0)}}</div>
+          <div>{{localize('bananas', 'name', 'Robin', 'gender', 'other', 'num', 4)}}</div>
+        </div>
+        <div class="code-container">
+          <code>localize('fruit', 'num', 0) / localize('fruit', 'num', 1) / localize('fruit', 'num', 3)</code>
+          <code>localize('bananas', 'name', 'Robin', 'gender', 'male', 'num', 1)</code>
+          <code>localize('bananas', 'name', 'Robin', 'gender', 'female', 'num', 0)</code>
+          <code>localize('bananas', 'name', 'Robin', 'gender', 'other', 'num', 4)</code>
+        </div>
       </div>
-      <div class="code-container">
-        <code>localize('fruit', 'num', 0) / localize('fruit', 'num', 1) / localize('fruit', 'num', 3)</code>
-        <code>localize('bananas', 'name', 'Robin', 'gender', 'male', 'num', 1)</code>
-        <code>localize('bananas', 'name', 'Robin', 'gender', 'female', 'num', 0)</code>
-        <code>localize('bananas', 'name', 'Robin', 'gender', 'other', 'num', 4)</code>
-      </div>
-    </div>
-`,
+    `;
+  }
 
-  is: 'x-translate',
+  static get properties() {
+    return {
+      /* Overriden from AppLocalizeBehavior */
+      language: {value: 'en', type: String},
 
-  behaviors: [AppLocalizeBehavior],
-
-  properties: {
-    /* Overriden from AppLocalizeBehavior */
-    language: {value: 'en', type: String},
-
-    /* Overriden from AppLocalizeBehavior */
-    formats: {
-      type: Object,
-      value: function() {
-        return {number: {USD: {style: 'currency', currency: 'USD'}}};
+      /* Overriden from AppLocalizeBehavior */
+      formats: {
+        type: Object,
+        value: function() {
+          return {number: {USD: {style: 'currency', currency: 'USD'}}};
+        }
       }
-    }
-  },
+    };
+  }
 
-  attached: function() {
+  connectedCallback() {
+    super.connectedCallback();
     this.loadResources(this.resolveUrl('locales.json'));
-  },
+  }
 
-  _toggle: function() {
+  _toggle() {
     this.language = this.$.switch.checked ? 'fr' : 'en';
   }
-});
+}
+
+window.customElements.define('x-translate', xTranslate);
